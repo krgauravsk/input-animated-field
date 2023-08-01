bb330ce435291c7733582614f9a8fbc2c66e652a
Kumar Gaurav <66477464+krgauravsk@users.noreply.github.com>
2023-08-01 22:09:33 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      {/* Input animated field */}
+
+      <div class="input-data">
+        <input type="text" required />
+        {/* <div class="underline" /> */}
+        <label>First Name</label>
+      </div>
+    </div>
+  );
+}
